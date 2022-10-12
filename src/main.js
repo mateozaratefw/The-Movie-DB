60ac7e7a9c56ffc5289dd623f4e639d2e141d434
Mateo Zarate <mateozaratefw@gmail.com>
2022-10-12 14:51:02 -0300

    API CATEGORIES DONE :)

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ async function getTrendingMoviesPreview() {
 
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
-
         const movieImg = document.createElement('img');
         movieImg.classList.add('movie-img');
         movieImg.setAttribute('alt', movie.title);
@@ -16,9 +15,33 @@ async function getTrendingMoviesPreview() {
             'src', 
             'https://image.tmdb.org/t/p/w300/' + movie.poster_path,
         );
+
         movieContainer.appendChild(movieImg);
         trendingPreviewMoviesContainer.appendChild(movieContainer)
     });
 }
 
+async function getCategoriesMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await res.json();
+    const categories = data.genres;
+    
+    categories.forEach(category => {
+        const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+
+        const categorytitle = document.createElement('h3');
+        categorytitle.classList.add('category-title');
+        categorytitle.setAttribute('id', 'id' + category.id);
+        const categoryTitleText = document.createTextNode(category.name);
+        
+        categorytitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categorytitle);
+        previewCategoriesContainer.appendChild(categoryContainer);
+    });
+}
+
 getTrendingMoviesPreview();
+getCategoriesMoviesPreview();
